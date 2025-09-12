528fbb5e002eae4fd023a74d58c497fb712ef074
johnodon <johnodon@hotmail.com>
2025-09-12 14:08:52 -0400

    Create amd_vulkan.sh

---
+++ amd_vulkan.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Enable repositories and install drivers
+sudo add-apt-repository ppa:oibaf/graphics-drivers -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils
+
