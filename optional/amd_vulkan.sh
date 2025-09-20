55f4b8500396ccabe05b2c70fc65747c545fca5d
johnodon <johnodon@hotmail.com>
2025-09-20 10:33:05 -0400

    Update amd_vulkan.sh

--- optional/amd_vulkan.sh
+++ optional/amd_vulkan.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # Enable repositories and install drivers
 sudo add-apt-repository ppa:oibaf/graphics-drivers -y
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils mesa-utils
+sudo apt install libvulkan1 mesa-vulkan-drivers mesa-utils -y
 
